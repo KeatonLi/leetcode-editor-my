40e629c1c082d5b5f847b124ecea49d4bb53a5e0
libokai <li_bo_kai@qq.com>
2025-06-08 11:15:18 +0800

    init repo

---
+++ T1.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class T1 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public List<Integer> lexicalOrder(int n) {
+        List<Integer> res = new ArrayList<>();
+        return res;
+    }
+}
