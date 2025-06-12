95c2168b38d4ab2907ea21217f9d2625cf97fabb
libokai <li_bo_kai@qq.com>
2025-06-12 10:50:12 +0800

    feat: hard难题解决

--- leetcode/editor/cn/奇偶频次间的最大差值 II.java
+++ leetcode/editor/cn/奇偶频次间的最大差值 II.java
@@ -4,17 +4,29 @@ class 奇偶频次间的最大差值 II{
 
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
-        public int maxDifference(String S, int k) {
-            int n = S.length();
+        public int maxDifference(String s, int k) {
+            int n = s.length();
+            int ans = -40000;
             for (int i = 0; i < 5; i++) {
                 for (int j = 0; j < 5; j++) {
-
-
-                    for (int l = 0, r = 0; r < n; r++) {
-
+                    int[] prefixRight = new int[5];
+                    int[] prefixLeft = new int[5];
+                    int[][] max = new int[][]{{-40000, -40000}, {-40000, -40000}};
+                    for (int l = 0, m = 0; m < n; m++) {
+                        int r = m + 1;
+                        prefixRight[s.charAt(m) - '0']++;
+                        while (r - l >= k && prefixRight[i] > prefixLeft[i] && prefixRight[j] > prefixLeft[j]) {
+                            int z = prefixLeft[i] & 1;
+                            int x = prefixLeft[j] & 1;
+                            max[z][x] = Math.max(max[z][x], prefixLeft[j] - prefixLeft[i]);
+                            prefixLeft[s.charAt(l) - '0']++;
+                            l++;
+                        }
+                        ans = Math.max(ans, prefixRight[i] - prefixRight[j] + max[prefixRight[i] & 1 ^ 1][prefixRight[j] & 1]);
                     }
                 }
             }
+            return ans;
         }
     }
 
